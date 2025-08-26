89e05596c3191cc434a797fb380e561968a920ea
szilavi <szilagyi.viktor89@gmail.com>
2025-08-26 10:58:55 +0200

    update3

--- assets/app/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var toastEl = document.getElementById("toaster");
-  if (toastEl) {
-    var toast = new bootstrap.Toast(toastEl, { delay: 3500 });
-    toast.show();
-  }
-});
